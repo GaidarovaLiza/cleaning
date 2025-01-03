b5668aebb4a85d3ec6b57c13002f267d76c1fb89
Ivan Zhuravlev <i.zhurravlev@gmail.com>
2025-01-03 23:07:37 +0300

    added service card realization, change additional.ts and additional items

---
+++ src/components/serviceCard/serviceCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './ServiceCard.module.scss';
+
+interface ServiceCardProps {
+    src: string;
+    alt: string;
+    text: string;
+    price: string;
+    isSelected: boolean;
+    onClick: () => void;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ src, alt, text, price, isSelected, onClick }) => {
+    return (
+      <div
+        className={`${styles.card} ${isSelected ? styles.selected : ''}`}
+        onClick={onClick}
+        style={{ backgroundImage: `url(${src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+        aria-label={alt}
+      >
+          <div className={styles.infoContainer}>
+              <div className={styles.textContainer}>
+                  <div className={styles.text}>
+                      {text}
+                  </div>
+              </div>
+              <div className={styles.priceContainer}>
+                      +{price}
+              </div>
+          </div>
+      </div>
+    );
+};
+
+export default ServiceCard;
--- src/views/tariffs/additionalItems/AdditionalItems.tsx
+++ src/views/tariffs/additionalItems/AdditionalItems.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
-import { Grid } from '@mui/material';
 import { useAdditionalItemsStore, useRoomCountStore } from '../../../store/store';
 import { Typography } from '../../../components/typography';
 import { iconList } from '../../../data/additionals';
 
 import style from './AdditionalItems.module.scss';
+import ServiceCard from '../../../components/serviceCard/serviceCard';
 
 export const AdditionalItems = () => {
   const { addMaintenancePrice } = useRoomCountStore();
@@ -27,29 +27,19 @@ export const AdditionalItems = () => {
       <Typography className={style.wrapper_text} variant="h1">
         Выберите опции для Вашей уборки:
       </Typography>
-      <Grid className={style.grid} container spacing={1} columns={{ xs: 2, sm: 4, md: 12 }}>
-        {iconList.map(icon => (
-          <Grid
+      <div className={style.grid}>
+        {iconList.map((icon) => (
+          <ServiceCard
             key={icon.alt}
-            item
-            xs={0}
-            sm={2}
-            md={4}
-            sx={{ paddingLeft: 2 }}
-            className={icon.isAdded ? `${style.gridItem} ${style.selectedItem}` : style.gridItem}
-          >
-            <div className={style.link} onClick={() => handleClick(icon)}>
-              <img className={style.img} src={icon.src} alt={icon.alt} />
-              <Typography className={style.text} variant="semiBold">
-                {icon.text}
-              </Typography>
-              <div className={style.priceBlock}>
-                <Typography variant="semiBold">{icon.price}</Typography>
-              </div>
-            </div>
-          </Grid>
+            src={icon.src}
+            alt={icon.alt}
+            text={icon.text}
+            price={`${icon.price}`}
+            isSelected={icon.isAdded}
+            onClick={() => handleClick(icon)}
+          />
         ))}
-      </Grid>
+      </div>
     </div>
   );
 };
