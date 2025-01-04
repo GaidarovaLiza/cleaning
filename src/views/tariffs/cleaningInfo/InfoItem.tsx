9053c05a87d5886b7dfa787dec3f966d7294315f
Ivan31122003 <Ivan31122003@mail.ru>
2025-01-04 19:33:44 +0400

    Changed info block style

--- src/views/tariffs/cleaningInfo/InfoItem.tsx
+++ src/views/tariffs/cleaningInfo/InfoItem.tsx
@@ -1,18 +1,38 @@
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode, useEffect, useRef, useState } from 'react';
 import { Typography } from '../../../components/typography';
 
 import styles from './InfoItem.module.scss';
 
 export interface TypographyProps {
-  title: string;
+  title: ReactNode;
   text: string;
 }
 
-export const InfoItem = forwardRef<HTMLDivElement, TypographyProps>(({ title, text }) => {
+export const InfoItem = forwardRef<HTMLDivElement, TypographyProps>(({ title, text }, ref) => {
+  const [highlighted, setIsActive] = useState(false);
+  const itemRef = useRef<HTMLDivElement | null>(null); // локальный ref для элемента
+
+  const checkVisibility = () => {
+    if (itemRef.current) {
+      const rect = itemRef.current.getBoundingClientRect();
+      const isVisible = rect.top >= 0.5 * window.innerHeight && rect.top <= 0.6 * window.innerHeight;
+      setIsActive(isVisible);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkVisibility);
+    checkVisibility();
+
+    return () => {
+      window.removeEventListener('scroll', checkVisibility);
+    };
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Typography variant={'h3'} children={title} className={styles.container_title} />
-      <Typography variant={'medium'} children={text} className={styles.container_text} />
+    <div ref={itemRef} className={`${styles.container} ${highlighted ? styles.highlighted : ''}`}>
+      <Typography ref={ref} variant={'h3'} children={title} className={styles.container_title} />
+      <Typography ref={ref} variant={'medium'} children={text} className={styles.container_text} />
     </div>
   );
 });
