a2cecb50cbfbf763cce2d979cc1badb8e5bbface
GaidarovaLiza <gaidarova.liza@gmail.com>
2025-02-09 20:41:14 +0300

    finish card styling

--- src/views/pricingByRoom/PricingByRoom.tsx
+++ src/views/pricingByRoom/PricingByRoom.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Switch } from './switch/Switch';
 import { Typography } from 'src/components/typography';
 import RoomCard from './roomCard/RoomCard';
+import { oneRoomPrice, threeRoomPrice, twoRoomPrice } from 'src/data/roomPricingOption';
 
 import style from './PricingByRoom.module.scss';
 
@@ -9,18 +10,37 @@ const PricingByRoom = () => {
   const [active, setActive] = useState<boolean>();
   return (
     <div className={style.blockWrapper}>
-      <Typography variant="h1">
+      <Typography variant="xxxl-semibold">
         <span className={style.color}> Стоимость услуг</span> в зависимости <br /> от количества комнат
       </Typography>
       <div className={style.blockWrapper_textWrapper}>
-        <Typography variant="kgkgk">Поддерживающая уборка</Typography>
+        <Typography variant="l-regular" className={`${!active ? style.bold : ''}`}>
+          Поддерживающая уборка
+        </Typography>
         <Switch value={active} onChange={() => setActive(!active)} />
-        <Typography variant="h3">Генеральная уборка</Typography>
+        <Typography variant="l-regular" className={`${active ? style.bold : ''}`}>
+          Генеральная уборка
+        </Typography>
       </div>
       <div className={style.blockWrapper_cards}>
-        <RoomCard />
-        <RoomCard />
-        <RoomCard />
+        <RoomCard
+          title="Однокомнатная квартира"
+          discontPrice="72 BYN"
+          realPrice="84 BYN"
+          includedOption={oneRoomPrice}
+        />
+        <RoomCard
+          title="Двухкомнатная квартира"
+          discontPrice="96 BYN"
+          realPrice="110 BYN"
+          includedOption={twoRoomPrice}
+        />
+        <RoomCard
+          title="Трехкомнатная квартира"
+          discontPrice="120 BYN"
+          realPrice="145 BYN"
+          includedOption={threeRoomPrice}
+        />
       </div>
     </div>
   );
